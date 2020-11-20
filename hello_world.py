2b2fbd30120a1cff8f090409290ab3882e1e05be
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-20 02:31:05 +0000

    added hello_world.py with proper style

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on November 19 2020
+# This is the "Hello, World!" program with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
